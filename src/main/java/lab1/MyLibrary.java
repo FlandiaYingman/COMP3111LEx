7b3b44666b1ac56522bf61a5bcd4a3c65c55f814
Flandia Yingman <me@flandia.dev>
2025-09-18 18:33:30 +0800

    Lab1

---
+++ src/main/java/lab1/MyLibrary.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class MyLibrary {
+    public static int power(int base, int exponent) {
+        if (exponent == 1) {
+            return base * exponent;
+        } else {
+            return base * power(base, exponent - 1);
+        }
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
